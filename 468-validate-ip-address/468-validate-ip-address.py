33ca3fefd7295452aee0e79ef89a4ff943ec2d59
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-03-12 01:03:18 +0530

    Time: 32 ms (85.40%), Space: 13.8 MB (90.65%) - LeetHub

---
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validIPAddress(self, queryIP: str) -> str:
+        if(len(queryIP.split('.')) == 4 and all(self.is_valid_IPV4(bits) for bits in queryIP.split('.'))):
+            return "IPv4"
+        if(len(queryIP.split(':')) == 8 and all(self.is_valid_IPV6(bits) for bits in queryIP.split(':'))):
+            print(all(self.is_valid_IPV6(bits) for bits in queryIP.split(':')))
+            return "IPv6"
+        return "Neither"
+            
+    
+    def is_valid_IPV4(self,bits: str) -> bool:
+        try:
+            return str(int(bits)) == bits and 0 <= int(bits) <= 255
+        except:
+            return False
+            
+        
+    def is_valid_IPV6(self,bits: str) -> bool:
+        validAlphArea = ["a","A","b","B","c","C","d","D","e","E","f","F","1","2","3","4","5","6","7","8","9","0","-"]
+        try:
+            if(1 <= len(bits) <= 4):
+                if(all(bit in validAlphArea  for bit in bits)):
+                    return True
+            else:
+                return False
+        except:
+            return False
+            
+    
+    
+        
