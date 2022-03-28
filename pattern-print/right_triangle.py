6fafdfbfe13b8a3670288f1755eca06d4e5a5a6e
danish45007 <danish45007@gmail.com>
2022-03-28 12:09:11 +0530

    added pattern print problems

---
+++ pattern-print/right_triangle.py
@@ -0,0 +1,14 @@
+def print_right_triangle(n):
+   for i in range(0, n):
+     
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
+         
+            # printing stars
+            print("* ",end="")
+      
+        # ending line after each row
+        print("\r")
+
+print(print_right_triangle(6))
