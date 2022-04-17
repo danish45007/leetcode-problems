8221164a12b1c08f03193b675ab6b1e209bb49fe
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-04-18 03:32:14 +0530

    Time: 771 ms (96.11%), Space: 39.1 MB (75.26%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -21,6 +21,8 @@ class Solution:
             slow = nxt
         # start comparing the value from the start and end
         start,end = head,prev
+        # as the head of the second half is pointing to None and its a mid
+        # so if end reaches mid means we have traversed the half of the linked list
         while end:
             if start.val != end.val:
                 return False
