78c0ce3f42be821912188576b7c4d6cab53e365b
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-04-19 16:33:48 +0530

    Time: 184 ms (19.77%), Space: 19.5 MB (15.10%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,13 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        _map = {}
+        # count to value map
+        # key will the possible counts in array and value as array of vlaues against count
+        hash_map = {}
         res = []
         for num in nums:
-            _map[num] = 1 + _map.get(num,0)
-        heap = [(val,key) for key,val in _map.items()]
-        for val,key in (sorted(heap,reverse=True)[0:k]):
-            res.append(key)
-        return res
+            hash_map[num] = 1 + hash_map.get(num,0)
+        count_map = [[] for i in range(len(nums)+1)]
+        for key,val in hash_map.items():
+            count_map[val].append(key)
+        for i in range(len(count_map)-1,0,-1):
+            if len(count_map[i]) > 0:
+                for n in count_map[i]:
+                    res.append(n)
+                if len(res) == k:
+                    return res
+            
+            
+                
+            
+            
+            
+        
         
         
             
