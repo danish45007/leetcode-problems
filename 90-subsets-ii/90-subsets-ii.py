e01d9c932803de30827de3966214090f71c31091
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-04-21 00:28:43 +0530

    Time: 57 ms (42.07%), Space: 14.5 MB (11.04%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,18 +1,29 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         result = []
-        return self.solve(nums,[],result)
-    def solve(self,i,o,res):
-        if len(i) == 0:
-            if sorted(o) not in res:
-                res.append(sorted(o))
-            return
-        o1 = o.copy()
-        o2 = o.copy()
-        o2.append(i[0])
-        i = i[1:]
-        self.solve(i,o1,res)
-        self.solve(i,o2,res)
-        return res
-        
-        
+        subset = []
+        # using dfs
+        def dfs(i):
+            # out of bound
+            # reached a leaf of the decision tree
+            if(i >= len(nums)):
+                result.append(subset.copy())
+                return
+                    
+            # apply dfs on the left sub tree
+            # on the left side we include the current value into our subset
+            subset.append(nums[i])
+            # call on next input
+            dfs(i+1)
+            # apply dfs on the right sub tree
+            # on the right side we don't include the current value in our subset
+            
+            # poping out the value that was added while calling the left tree
+            subset.pop()
+            # call on the next input
+            dfs(i+1)
+        dfs(0)
+        # duplicate check
+        return list(set(tuple(sorted(r)) for r in result))
+                    
+            
