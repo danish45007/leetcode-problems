2e618cadee240bea2bf2469a29bacf538215b55e
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-04-21 15:58:32 +0530

    Time: 5433 ms (5.07%), Space: 516 MB (5.85%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -7,15 +7,23 @@ class Solution:
     
     @cache
     def backtrack(self, i, k):
+        memo = {}
         if i>=k:
             return 0
-        min_cuts = len(self.s)
+        min_cuts = float('inf')
         for j in range(i, k+1):
-            if self.valid(i, j):
+            if (i,j) in memo:
                 if j<k:
                     min_cuts = min(min_cuts, 1 + self.backtrack(j+1, k))
                 else:
                     min_cuts = min(min_cuts, 0)
+            else:
+                if self.valid(i, j):
+                    memo[(i,i)] = True
+                    if j<k:
+                        min_cuts = min(min_cuts, 1 + self.backtrack(j+1, k))
+                    else:
+                        min_cuts = min(min_cuts, 0)
         return min_cuts
 
     def minCut(self, s: str) -> int:
