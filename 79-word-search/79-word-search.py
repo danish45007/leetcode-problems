c63c101a1f08bc484a4f823a0a17df90c5337cb5
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-01 00:51:20 +0530

    Time: 7508 ms (41.08%), Space: 14.1 MB (13.89%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,21 +1,22 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row,col = len(board),len(board[0])
-        path = set()
+        visited = set()
         def dfs(r,c,i):
+            # found all char in target word
             if i == len(word):
                 return True
-            if r < 0 or c < 0 or r >= row or c >= col or board[r][c] != word[i] or (r,c) in path:
+            if r < 0 or c < 0 or r >= row or c >= col or board[r][c] != word[i] or (r,c) in visited:
                 return False
-            path.add((r,c))
-            res = (dfs(r,c+1,i+1) or
-            dfs(r,c-1,i+1) or
-            dfs(r+1,c,i+1) or
-            dfs(r-1,c,i+1))
-            path.remove((r,c))
+            visited.add((r,c))
+            res = (dfs(r,c+1,i+1) or dfs(r,c-1,i+1) or dfs(r+1,c,i+1) or dfs(r-1,c,i+1))
+            # remove the curr r,c from visited after making 4 calls
+            visited.remove((r,c))
             return res
             
+        
         for r in range(row):
             for c in range(col):
-                if dfs(r,c,0):return True
+                if dfs(r,c,0):
+                    return True
         return False
