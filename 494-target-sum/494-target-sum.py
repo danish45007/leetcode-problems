b3224ca81b8fd97437bb409fde710e941f2d7c41
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-03 16:17:03 +0530

    Time: 658 ms (15.24%), Space: 36 MB (26.59%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,18 +1,16 @@
 class Solution:
     def findTargetSumWays(self, arr: List[int], target: int) -> int:
-        arr_sum = sum(arr)
-        if(len(arr) == 1 and target > arr[0] or (arr_sum + target) % 2 != 0 or abs(target) > arr_sum):
-            return 0
-        target_sum = (arr_sum + target) // 2
-        n = len(arr)
-        dp = [[0 for j in range(target_sum+1)] for i in range(n+1)]
-        dp[0][0] = 1
-        for j in range(1,target_sum+1):
-            dp[0][j] = 0
-        for i in range(1,n+1):
-            for j in range(target_sum+1):
-                if(arr[i-1] > j):
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]]
-        return dp[n][target_sum]
+        dp = {}
+        def dfs(index,curr_sum):
+            if index == len(arr):
+                return 1 if curr_sum == target else 0
+                
+            if (index,curr_sum) in dp:
+                return dp[(index,curr_sum)]
+            
+            res = dfs(index+1,curr_sum+arr[index]) + dfs(index+1,curr_sum-arr[index])
+            dp[(index,curr_sum)] = res
+            
+            return dp[(index,curr_sum)]
+        return dfs(0,0)
+                
