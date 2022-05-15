661fcc2fe89d1da9a871aeeb85b931b67380f9cb
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-16 01:41:04 +0530

    Time: 72 ms (58.09%), Space: 14.3 MB (14.38%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num_index_map = {}
+        res = [-1]*len(nums1)
+        for i,num in enumerate(nums1):
+            num_index_map[num] = i
+        for i,n in enumerate(nums2):
+            if n in num_index_map:
+                curr_index = i
+                while i < len(nums2)-1:
+                    # nearest max
+                    if nums2[i+1] > n:
+                        res[num_index_map[n]] = nums2[i+1]
+                        break
+                    else:
+                        res[num_index_map[n]] = -1
+                    i += 1
+        return res
+                        
