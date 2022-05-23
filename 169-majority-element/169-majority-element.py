52c14d609658fe6597c779cb1e22d40d18ff7d82
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-23 14:22:42 +0530

    Time: 249 ms (34.04%), Space: 15.5 MB (35.41%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,9 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        
         # using linear time and space
-        _map = {}
-        for num in nums:
-            _map[num] = 1 + _map.get(num,0)
-        for n,count in _map.items():
-            if count > len(nums) // 2:
-                return n
+        # _map = {}
+        # for num in nums:
+        #     _map[num] = 1 + _map.get(num,0)
+        # for n,count in _map.items():
+        #     if count > len(nums) // 2:
+        #         return n
+        
+        
+        # using constant space and nlogn time
+        nums.sort()
+        prev = nums[0]
+        if len(nums) == 1:
+            return prev
+        res = 1
+        for i in range(1,len(nums)):
+            if nums[i] == prev:
+                res += 1
+            else:
+                prev = nums[i]
+                res = 1
+            
+            if res > len(nums) // 2:
+                return nums[i]
+            
