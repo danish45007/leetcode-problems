bbf71d648d541f84f65b2a4b4356e3e336e67cf2
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-25 01:32:24 +0530

    Time: 52 ms (28.72%), Space: 14 MB (10.76%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,21 +1,21 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         # brute force O(sqrt(n))
-        for i in range(1,num+1):
-            if i*i == num:
-                return True
-            # return False immediately
-            if i*i > num:
-                return False
-        # binary search logn solution
-        # start = 1
-        # end = num
-        # while start <= end:
-        #     mid = (start + end) // 2
-        #     if mid*mid == num:
+        # for i in range(1,num+1):
+        #     if i*i == num:
         #         return True
-        #     elif mid*mid > num:
-        #         end = mid - 1
-        #     elif mid*mid < num:
-        #         start = mid + 1
-        # return False
+        #     # return False immediately
+        #     if i*i > num:
+        #         return False
+        # binary search logn solution
+        start = 1
+        end = num
+        while start <= end:
+            mid = (start + end) // 2
+            if mid*mid == num:
+                return True
+            elif mid*mid > num:
+                end = mid - 1
+            elif mid*mid < num:
+                start = mid + 1
+        return False
