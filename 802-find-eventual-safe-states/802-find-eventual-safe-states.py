42d7696be7598e48624de640fbfb23abc7a0e9ad
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-25 03:39:36 +0530

    Time: 703 ms (83.26%), Space: 21.8 MB (52.53%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -0,0 +1,23 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        safe = {}
+        res = []
+        n = len(graph)
+        def dfs(node):
+            if node in safe:
+                return safe[node]
+            
+            # marking the node not safe before visting its neighbours
+            safe[node] = False
+            # checking all the neighbours of the passed node
+            # if atleast one of the neighbour is not safe mark it False
+            for neighbour in graph[node]:
+                if not dfs(neighbour):
+                    return safe[node]
+            safe[node] = True
+            return safe[node]
+        
+        for node in range(n):
+            if dfs(node):
+                res.append(node)
+        return res
