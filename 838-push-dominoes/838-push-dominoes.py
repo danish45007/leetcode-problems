fa310b794b8d247041b764e13194a813f2bc5b0f
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-26 03:23:34 +0530

    Time: 414 ms (47.04%), Space: 22.5 MB (16.38%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.py
+++ 838-push-dominoes/838-push-dominoes.py
@@ -1,7 +1,10 @@
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
+        # converting the dominoes<type:str> into <type:array> to able to mutate values at indices
         dominoes = list(dominoes)
-        queue = deque()
+        queue = deque() #[index,state(left/right)]
+        # adding non standing dominoes (left/right) into queue
+        # as these are responsible for changing the state of other dominoes
         for index,dom in enumerate(dominoes):
             if dom != '.':
                 queue.append((index,dom))
@@ -10,15 +13,23 @@ class Solution:
             index,dom = queue.popleft()
             # in case of left falling
             if dom == "L":
+                # checking the prev dominoe if standing then standing its state to left
                 if index-1 >= 0 and dominoes[index-1] == ".":
+                    # adding prev left falling dominoes into queue as it can be responsible for chaning the state of its prev dominoe
                     queue.append((index-1,"L"))
                     dominoes[index-1] = "L"
+            # in case of left falling
             elif dom == "R":
+                # case Right -> Standing <- Left
+                # will have no impact on the state of 3 contiguous dominoe
                 if(index+1 < len(dominoes) and index+2 < len(dominoes) and dominoes[index+1] == '.' and dominoes[index+2] == "L"):
+                    # need to pop out the one falling toward left as its been proccessed
                     queue.popleft()
+                # case Right -> Standing similar the left falling
                 elif index+1 < len(dominoes) and dominoes[index+1] == ".":
                     queue.append((index+1,"R"))
                     dominoes[index+1] = "R"
+        # cnverting back array into str type
         return "".join(dominoes)
                     
                     
