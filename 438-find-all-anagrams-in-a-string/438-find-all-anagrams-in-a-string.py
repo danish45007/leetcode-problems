8fef412509892756998c0a682f51d3a29d2f01b7
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-25 14:08:02 +0530

    Time: 137 ms (76.96%), Space: 15.2 MB (33.51%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,34 +1,35 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> int:
-        return self.search(p,s)
+        return self.sliding_window_solution(p,s)
     
-    def search(self,pat, txt):
+    def sliding_window_solution(self,pattern, txt):
         i,j = 0,0
         res = []
-        pattern_map = {}
-        for char in pat:
-            if(char in pattern_map):
-                pattern_map[char] += 1
-            else:
-                pattern_map[char] = 1
+        pattern_map = {} #{char: count}
+        for char in pattern:
+            pattern_map[char] = 1 + pattern_map.get(char,0)
         count = len(pattern_map)
-        k = len(pat)
+        # window size
+        k = len(pattern)
         while j < len(txt):
-            if(txt[j] in pattern_map):
+            # if current char in map reduce the count
+            if txt[j] in pattern_map:
                 pattern_map[txt[j]] -=1
-                if(pattern_map[txt[j]] == 0):
-                    count -=1 
-            if(j-i+1 < k):
-                j +=1
-            elif(j-i+1 == k):
-                if(count == 0):
+                # no char in for the next window
+                if pattern_map[txt[j]] == 0:
+                    count -=1
+            # reached the window
+            if j-i+1 == k:
+                if count == 0:
                     res.append(i)
-                if(txt[i] in pattern_map):
-                    pattern_map[txt[i]] +=1
-                    if(pattern_map[txt[i]] == 1):
-                        count +=1
-                i +=1
-                j +=1
+                if txt[i] in pattern_map:
+                    pattern_map[txt[i]] += 1
+                    if pattern_map[txt[i]] == 1:
+                        count+=1
+                i+=1
+            j +=1
         return res
+            
+                
             
     
