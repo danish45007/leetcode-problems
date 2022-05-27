0a1b8e3d5d2b1275a40412aff065fbff853d889f
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-27 23:07:34 +0530

    Time: 100 ms (50.09%), Space: 14.4 MB (88.16%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,19 +1,40 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key=lambda t: t[1])
-        min_heap = [] # [end,no_of_pes]
-        curr_pes = 0
+        # O(n)
+        # total passengers from all start to end
+        change_in_passengers = [0]*1001
         for t in trips:
-            pes,start,end = t
-            curr_pes += pes
-            while min_heap and min_heap[0][0] <= start:
-                last_end,last_pes = heapq.heappop(min_heap)
-                curr_pes -= last_pes
-            if(curr_pes > capacity):
+            pessengers,start,end = t
+            # pessengers will be added at start index
+            change_in_passengers[start] += pessengers
+            # pessengers will be removed at end index
+            change_in_passengers[end] -= pessengers
+        
+        # checking pessengers for the each index
+        curr_pessengers = 0
+        for i in range(1001):
+            curr_pessengers += change_in_passengers[i]
+            if curr_pessengers > capacity:
                 return False
-            heapq.heappush(min_heap,[end,pes])
-
         return True
+            
+        
+    
+        # O(nlogn)
+#         trips.sort(key=lambda t: t[1])
+#         min_heap = [] # [end,no_of_pes]
+#         curr_pes = 0
+#         for t in trips:
+#             pes,start,end = t
+#             curr_pes += pes
+#             while min_heap and min_heap[0][0] <= start:
+#                 last_end,last_pes = heapq.heappop(min_heap)
+#                 curr_pes -= last_pes
+#             if(curr_pes > capacity):
+#                 return False
+#             heapq.heappush(min_heap,[end,pes])
+
+#         return True
                 
                 
         
