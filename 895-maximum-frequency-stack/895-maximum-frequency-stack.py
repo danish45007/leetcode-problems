740df7090f344bf0829c1a3401f8aa2e19d35397
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-31 03:43:17 +0530

    Time: 332 ms (94.02%), Space: 23 MB (20.05%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.py
@@ -0,0 +1,29 @@
+class FreqStack:
+
+    def __init__(self):
+        self.freq_count = {}
+        self.max_count = 0
+        self.stacks = {}
+
+    def push(self, val: int) -> None:
+        val_count = 1 + self.freq_count.get(val,0)
+        self.freq_count[val] = val_count
+        # construct a new stack against val_count
+        if val_count > self.max_count:
+            self.max_count = val_count
+            self.stacks[val_count] = []
+        self.stacks[val_count].append(val)
+
+
+    def pop(self) -> int:
+        res = self.stacks[self.max_count].pop()
+        self.freq_count[res] -=1
+        if len(self.stacks[self.max_count]) == 0:
+            self.max_count -=1
+        return res
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
