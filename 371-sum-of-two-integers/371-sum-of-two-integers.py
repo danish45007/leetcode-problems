07a1d68683120bb7852c639bcb0c31862a1a6e9a
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-31 20:37:21 +0530

    Time: 37 ms (62.65%), Space: 14 MB (13.32%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xffffffff
+
+        while b != 0:
+            tmp = (a & b) << 1
+            a = (a ^ b) & mask
+            b = tmp & mask
+
+        if a > mask // 2:
+            return ~(a ^ mask)
+        else:
+            return a
