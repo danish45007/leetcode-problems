88fbbc37a31333efc7502fb3dbbf03f291ba96a9
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-31 16:08:37 +0530

    Time: 360 ms (37.77%), Space: 25.4 MB (29.14%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,8 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # using extra memory
         res = [0]*len(nums)
         for i in range(len(nums)):
             idx = (i+k)%len(nums)
             res[idx] = nums[i]
         nums[:] = res
+        
+        # without using extra memory
+        nums = list(reversed(nums))
+        kth_part = nums[:k+1]
+        non_kth_part = nums[k+1:]
+        nums = list(reversed(kth_part)) + list(reversed(non_kth_part))
+        
