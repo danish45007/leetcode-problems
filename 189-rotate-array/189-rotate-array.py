0c1c867aa7f2561961c65f47fccc1628ef96a2b3
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-31 16:20:38 +0530

    Time: 435 ms (25.84%), Space: 25.3 MB (76.58%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -4,15 +4,36 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # using extra memory
-        res = [0]*len(nums)
-        for i in range(len(nums)):
-            idx = (i+k)%len(nums)
-            res[idx] = nums[i]
-        nums[:] = res
+        # res = [0]*len(nums)
+        # for i in range(len(nums)):
+        #     idx = (i+k)%len(nums)
+        #     res[idx] = nums[i]
+        # nums[:] = res
         
         # without using extra memory
-        nums = list(reversed(nums))
-        kth_part = nums[:k+1]
-        non_kth_part = nums[k+1:]
-        nums = list(reversed(kth_part)) + list(reversed(non_kth_part))
+        # nums = list(reversed(nums))
+        # kth_part = nums[:k+1]
+        # non_kth_part = nums[k+1:]
+        # nums = list(reversed(kth_part)) + list(reversed(non_kth_part))
+        
+        # using no reversed keyword
+        def reverse(start,end):
+            while start < end:
+                nums[start],nums[end] = nums[end],nums[start]
+                start +=1
+                end -=1
+        k = k%len(nums)
+        # reverse the array
+        start,end = 0,len(nums)-1
+        reverse(start,end)
+        # reverse the kth portion
+        start,end=0,k-1
+        reverse(start,end)
+        
+        # reverse the non kth potion
+        start,end = k,len(nums)-1
+        reverse(start,end)
+        
+        
+                
         
