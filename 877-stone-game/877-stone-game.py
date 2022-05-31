9ecfc7c7fa843ffdc01974e4ec641d7308d8777c
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-05-31 18:57:33 +0530

    Time: 1170 ms (9.28%), Space: 125.9 MB (6.19%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -4,15 +4,36 @@ class Solution:
         # and as the overall sum of the stones is odd therefore sum of even != sum of odd
         # implies either even is greater or odd is greater
         
+        dp = {} # {subarray[l:r]: max of alice}
         
-        dp = {} #((l,r): max_sum)
+        # @ returns: max alice can pick
         def dfs(l,r):
-            if r - l == n:
-                return 
-            if (l,r) in dp :
-                return dp[(l,r)]
-        
-        return True
+            if l > r: return 0
+            if (l,r) in dp: return dp[(l,r)]
+            
+            # if alices start picking 
+            # in alices turn of picking there will be even stones in array
+            # in bob's turn there will be odd stones in array
+            is_even = True if (r-l)%2 else False
+            left_pick = piles[l] if is_even else 0
+            right_pick = piles[r] if is_even else 0
+            
+            # if alice's pick left stone
+            # dfs will be called on the remaining subarray
+            left = dfs(l+1,r) + left_pick
+            
+            # if alice's pick right stone
+            # dfs will be called on the remaining subarray
+            
+            right = dfs(l,r-1) + right_pick
+            
+            # consider the max of left and right
+            
+            dp[(l,r)] = max(left,right)
+            return dp[(l,r)]
+        return dfs(0,len(piles)-1) > sum(piles) // 2
+            
+            
             
             
                  
