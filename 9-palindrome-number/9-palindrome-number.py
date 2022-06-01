447d5c93bc89694c4aa11bbf40e68247d8b5c91e
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-02 02:10:23 +0530

    Time: 153 ms (5.36%), Space: 13.9 MB (59.34%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,10 +1,34 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        l,r = 0,len(x)-1
-        while l < r:
-            if x[l] != x[r]:
-                return False
-            l +=1
-            r -=1
+        # x = str(x)
+        # l,r = 0,len(x)-1
+        # while l < r:
+        #     if x[l] != x[r]:
+        #         return False
+        #     l +=1
+        #     r -=1
+        # return True
+        
+        # follow up without converting into string
+        if x < 0: return False
+        div = 1
+        while x >= div*10:
+            div = div*10
+
+        while x:
+            right_digit = x % 10
+            left_digit = x // div
+            if right_digit != left_digit: return False
+            
+            # moving left and right
+            # in this case trimming the curr left and right to update the new left and right
+            
+            # trimming left
+            x = x % div
+            # trimming left
+            x = x // 10
+            # updating div as 2 digits are removed
+            div = div / 100
         return True
+        
+        
