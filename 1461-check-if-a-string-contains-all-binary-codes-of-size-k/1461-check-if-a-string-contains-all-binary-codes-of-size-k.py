4ee9ca8644888e7df0859f9c5b5dad31a45cd646
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-02 15:59:56 +0530

    Time: 771 ms (26.34%), Space: 51.4 MB (7.04%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -12,3 +12,21 @@ class Solution:
                 l+=1
             r +=1
         return len(unique_code_set) == 2**k
+        
+        # optimization using rolling hash
+        '''
+            Time: O(n)
+            Space: O(2^k)
+        '''
+        # N = 2**k
+        # found = [False]*N
+        # all_one = N-1
+        # hash_val = 0
+        # for i in range(len(s)):
+        #     hash_val = ((hash_val << 1) and all_one) or (ord(s[i]) - ord('0'))
+        #     if i >= k-1 and not found[hash_val]:
+        #         found[hash_val] = True
+        #         N -= 1
+        #     if N == 0: return True
+        # return False
+                
