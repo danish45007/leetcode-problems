6cb7ce478af987c4dcb847e21f478a4fc11213a5
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-05 23:12:35 +0530

    Time: 4959 ms (15.98%), Space: 15.1 MB (53.25%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -5,17 +5,16 @@ class Solution:
         left = set()
         # as starting from the 1 as the mid of palindrome char at 0th index should be the part of the left
         # left.add(s[0])
-        res = set()
+        res = set() #(mid,right)
         right = {}
         for char in s:
             right[char] = 1 + right.get(char,0)
-        print(right)
         for m in range(len(s)):
             right[s[m]] -=1
             # checking all char a-z in the left and right
             for i in range(26):
                 char = chr(ord('a')+i)
-                if char in left and right[char] >0:
+                if char in left and right[char] > 0:
                     res.add((s[m],char))
             left.add(s[m])
         return len(res)
