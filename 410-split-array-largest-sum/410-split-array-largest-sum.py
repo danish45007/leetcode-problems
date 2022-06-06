7dae0b01a83ebd73a4880adef1f696b880aacde8
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-06 20:04:31 +0530

    Time: 45 ms (70.39%), Space: 13.9 MB (65.06%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,5 +1,27 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
+        # backtracking solution
+        # O(n^2*m)
+        # dp = {} #{indx,sub_set}
+        # def dfs(i,m):
+        #     if m == 1:
+        #         return sum(nums[i:])
+        #     if (i,m) in dp:
+        #         print((i,m))
+        #         return dp[(i,m)]
+        #     curr_sum = 0
+        #     res = float('inf')
+        #     for j in range(i,len(nums)-m+1):
+        #         curr_sum += nums[j]
+        #         max_sum = max(curr_sum,dfs(j+1,m-1))
+        #         res = min(res,max_sum)
+        #         if curr_sum > max_sum:
+        #             break
+        #     dp[(i,m)] = res
+        #     return res
+        # return dfs(0,m)
+    
+    
         start = max(nums)
         end = sum(nums)
         res = -1
@@ -14,12 +36,9 @@ class Solution:
     def is_valid_sub_array_sum(self,nums,m,curr_max):
         sub = 1
         _sum = 0
-        for i in range(len(nums)):
-            _sum += nums[i]
+        for num in nums:
+            _sum += num
             if(_sum > curr_max):
                 sub +=1
-                _sum = nums[i]
-        if(sub > m):
-            return False
-        else:
-            return True
+                _sum = num
+        return sub <= m
