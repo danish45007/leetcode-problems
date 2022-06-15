bc13399f35dc7b743899ef5273f0e631cc938fbd
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-15 22:09:05 +0530

    Time: 1544 ms (38.86%), Space: 169.2 MB (5.01%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        graph = defaultdict(list)
+        def dfs(node):
+            # if not node:
+            #     return
+            if node.left:
+                graph[node.val].append((node.left.val,'L'))
+                graph[node.left.val].append((node.val,'U'))
+                dfs(node.left)
+            if node.right:
+                graph[node.val].append((node.right.val,'R'))
+                graph[node.right.val].append((node.val,'U'))
+                dfs(node.right)
+        dfs(root)
+        queue = deque([(startValue,'')])
+        visited = set()
+        visited.add(startValue)
+        while queue:
+            node,res = queue.popleft()
+            if node == destValue:
+                return res
+            else:
+                for edge,d in graph[node]:
+                    if edge not in visited:
+                        visited.add(edge)
+                        queue.append((edge,res+d))
+        return -1
+                    
