496b9fe5162dc8f5c63e91fed518b672107383e3
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-22 02:24:48 +0530

    Time: 58 ms (32.02%), Space: 14 MB (27.89%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -1,25 +1,36 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        res = ""
-        char_count_map = {}
+        res = []
+        char_count = {}
         for char in s:
-            char_count_map[char] = 1 + char_count_map.get(char,0)
+            char_count[char] = 1 + char_count.get(char,0)
         
-        max_heap = [] #[count,char]
-        for char,count in char_count_map.items():
-            heapq.heappush(max_heap,[-count,char])
-        prev = None
-        while max_heap or prev:
-            if not max_heap and prev:
-                return ""
-            count,char = heapq.heappop(max_heap)
-            res += char
-            count +=1
-            if prev:
-                heapq.heappush(max_heap,prev)
-                prev = None
-            if count != 0:
-                prev = [count,char]
-        return res
+        max_heap = [] #[(char_count,char)]
+        for char,count in char_count.items():
+            heapq.heappush(max_heap,(-count,char))
+        
+        while len(max_heap) >= 2:
+            top_most_count,top_most_char = heapq.heappop(max_heap)
+            next_count,next_char = heapq.heappop(max_heap)
+            
+            res.append(top_most_char)
+            res.append(next_char)
+            
+            if top_most_count + 1 != 0:
+                heapq.heappush(max_heap,(top_most_count+1,top_most_char))
+            
+            if next_count + 1 != 0:
+                heapq.heappush(max_heap,(next_count+1,next_char))
+        
+        # only one odd char left to get processed
+        if max_heap:
+            char_count,char = heapq.heappop(max_heap)
+            # not a valid case to add into result
+            if -char_count > 1 or res[-1] == char:
+                return ''
+            res.append(char)
+        return ''.join(res)
+            
+            
             
             
