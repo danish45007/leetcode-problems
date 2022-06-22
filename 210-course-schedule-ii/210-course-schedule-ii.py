35c05cc4f60aafce228e1689548a49b9ad3d6e5a
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-23 02:08:01 +0530

    Time: 161 ms (42.98%), Space: 17.6 MB (10.74%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -3,24 +3,43 @@ class Solution:
         hash_map= {i:[] for i in range(numCourses)}
         for crs,pre in prerequisites:
             hash_map[crs].append(pre)
-        curr_cycle = set()
-        visited = set()
+        curr_cycle = set() # course in current cycle (ongoing dfs)
+        visited = set() # couse that has been visited
         res = []
-        def dfs(crs):
-            if crs in curr_cycle:
+        # def dfs(crs):
+        #     if crs in curr_cycle:
+        #         return False
+        #     if crs in visited:
+        #         return True
+        #     curr_cycle.add(crs)
+        #     for pre in hash_map[crs]:
+        #         if dfs(pre) == False:
+        #             return False
+        #     visited.add(crs)
+        #     curr_cycle.remove(crs)
+        #     res.append(crs)
+        #     return True
+        # for c in range(numCourses):
+        #     if dfs(c) == False:
+        #         return []
+        # return res
+        res = []
+        def dfs(course):
+            if course in curr_cycle:
                 return False
-            if crs in visited:
+            if course in visited:
                 return True
-            curr_cycle.add(crs)
-            for pre in hash_map[crs]:
-                if dfs(pre) == False:
+            curr_cycle.add(course)
+            for prereq_course in hash_map[course]:
+                if not dfs(prereq_course):
                     return False
-            visited.add(crs)
-            curr_cycle.remove(crs)
-            res.append(crs)
+            curr_cycle.remove(course)
+            visited.add(course)
+            res.append(course)
             return True
-        for c in range(numCourses):
-            if dfs(c) == False:
+        
+        for course in range(numCourses):
+            if not dfs(course):
                 return []
         return res
             
