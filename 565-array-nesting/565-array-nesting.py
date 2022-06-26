5df0bd5a9375a2a319a446ea8111ec9b84d22b59
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-26 16:17:35 +0530

    Time: 904 ms (95.29%), Space: 33.4 MB (34.12%) - LeetHub

--- 565-array-nesting/565-array-nesting.py
+++ 565-array-nesting/565-array-nesting.py
@@ -1,12 +1,25 @@
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
-        res = 0
+        # res = 0
+        # for i in range(len(nums)):
+        #     curr_len = 0
+        #     while nums[i] != -1:
+        #         temp = i
+        #         i = nums[i]
+        #         nums[temp] = -1
+        #         curr_len +=1
+        #     res = max(res,curr_len)
+        # return res
+        longest = 0
+        visited = set()
         for i in range(len(nums)):
-            curr_len = 0
-            while nums[i] != -1:
-                temp = i
-                i = nums[i]
-                nums[temp] = -1
-                curr_len +=1
-            res = max(res,curr_len)
-        return res
+            if nums[i] not in visited:
+                length = 0
+                idx = nums[i]
+                while idx != nums[i] or length == 0:
+                    length +=1
+                    visited.add(idx)
+                    idx = nums[idx]
+                longest = max(longest,length)
+        return longest
+                
