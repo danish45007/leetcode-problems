01a7000d63888e81068c0c652c8760462c2a9548
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-27 14:45:42 +0530

    Time: 134 ms (25.85%), Space: 37.5 MB (8.40%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
@@ -11,14 +11,16 @@ class Solution:
         def dfs(node,path):
             if not node:
                 return '#'
-            path += ','.join([str(node.val),dfs(node.left,path),dfs(node.right,path)])
+            left_path = dfs(node.left,path)
+            right_path = dfs(node.right,path)
+            path += ','.join([str(node.val),left_path,right_path])
             if path in tree_map:
                 tree_map[path] += 1
                 if tree_map[path] == 2:
                     res.append(node)
             else:
                 tree_map[path] = 1
-            
             return path
         dfs(root,'')
+        print(tree_map)
         return res
