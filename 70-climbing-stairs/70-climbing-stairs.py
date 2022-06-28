7d2c7c2e743c9749bab585d1a0b3cd1da713fe95
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-28 23:05:51 +0530

    Time: 56 ms (19.95%), Space: 13.9 MB (56.62%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,8 +2,8 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         dp = {}
         def helper(n):
-            if n == 0:
-                return 
+            if n <= 0:
+                return 0
             if n == 1:
                 return 1
             if n == 2:
