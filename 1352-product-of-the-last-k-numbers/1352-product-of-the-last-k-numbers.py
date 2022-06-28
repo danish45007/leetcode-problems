30b68dbc3d4a3350622b65a6e71af8b606c82d02
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-28 13:47:58 +0530

    Time: 530 ms (19.62%), Space: 29.3 MB (35.94%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -0,0 +1,27 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.products = []
+        self.product = 1
+
+    def add(self, num: int) -> None:
+        if num > 0:
+            self.product *= num
+            self.products.append(self.product)
+        else:
+            self.products = []
+            self.product = 1
+
+    def getProduct(self, k: int) -> int:
+        if len(self.products) < k:
+            return 0
+        elif len(self.products) == k:
+            return self.products[-1]
+        else:
+            return int(self.products[-1] / self.products[-1-k])
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
