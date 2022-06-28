5f976ad0caa13fc5f636e525df75094e84dd48ee
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-29 00:50:42 +0530

    Time: 64 ms (88.14%), Space: 14 MB (76.05%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,17 +1,23 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = {} #{step:min_cost}
-        def helper(n):
-            if n == 0:
-                return cost[0]
-            if n == 1:
-                return cost[1]
-            if n in dp:
-                return dp[n]
-            one_step_cost = helper(n-1)+cost[n]
-            two_step_cost = helper(n-2)+cost[n]
-            min_cost = min(one_step_cost,two_step_cost)
-            dp[n] = min_cost
-            return min_cost
-        return min(helper(len(cost)-1),helper(len(cost)-2))
-
+        # dp = {} #{step:min_cost}
+        # def helper(n):
+        #     if n == 0:
+        #         return cost[0]
+        #     if n == 1:
+        #         return cost[1]
+        #     if n in dp:
+        #         return dp[n]
+        #     one_step_cost = helper(n-1)+cost[n]
+        #     two_step_cost = helper(n-2)+cost[n]
+        #     min_cost = min(one_step_cost,two_step_cost)
+        #     dp[n] = min_cost
+        #     return min_cost
+        # return min(helper(len(cost)-1),helper(len(cost)-2))
+        
+        # bottom-up
+        cost.append(0)
+        for i in range(len(cost)-3,-1,-1):
+            cost[i] = min(cost[i]+cost[i+1],cost[i]+cost[i+2])
+        return min(cost[0],cost[1])
+        
