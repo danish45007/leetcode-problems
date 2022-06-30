dc4db48a8e9465195f1eb8c72bc6e0168aaa6b4d
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-06-30 22:13:04 +0530

    Time: 36 ms (85.68%), Space: 14.1 MB (28.73%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,17 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if len(s) == 0 or s[0] == '0':
-            return 0
-        if len(s) == 1:return 1
-        prev_count,prev_prev_count = 1,1
-        for i in range(1,len(s)):
-            single_digit = int(s[i])
-            double_digit = int(s[i-1:i+1])
-            curr_count = 0
-            if (single_digit > 0):curr_count += prev_count
-            if (double_digit >= 10 and double_digit <= 26):curr_count += prev_prev_count
-            prev_prev_count = prev_count
-            prev_count = curr_count
-        return prev_count
+        dp = {len(s): 1}
+        
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == '0':
+                return 0
+            res = dfs(i+1)
+            if (i+1) < len(s) and (s[i] == '1' or (s[i] == '2' and s[i+1] in '0123456')):
+                res += dfs(i+2)
+            dp[i] = res
+            return res
+        return dfs(0)
+                
                 
             
