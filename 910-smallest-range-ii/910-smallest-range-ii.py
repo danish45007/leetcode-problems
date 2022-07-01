04cbc80bb0d3d044ed479c02f8674838c30580bc
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-07-02 02:54:42 +0530

    Time: 178 ms (86.44%), Space: 15.2 MB (66.49%) - LeetHub

--- 910-smallest-range-ii/910-smallest-range-ii.py
+++ 910-smallest-range-ii/910-smallest-range-ii.py
@@ -3,6 +3,8 @@ class Solution:
         nums.sort()
         res = nums[-1] - nums[0]
         for i in range(len(nums)-1):
+            # assuming num[i] to be the max element in nums array and w.r.t trying to minimize the range diff. by calculating 
+            # the min value as close as to the num[i] + k (assumed max) 
             min_val = min(nums[0]+k,nums[i+1]-k)
             max_val = max(nums[i]+k,nums[-1]-k)
             res = min(res,max_val-min_val)
