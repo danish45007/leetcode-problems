a0612896e015b0ac4e57ee8a11c7934566e7032a
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-07-01 17:33:41 +0530

    Time: 759 ms (73.80%), Space: 15.3 MB (55.62%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.py
+++ 752-open-the-lock/752-open-the-lock.py
@@ -13,7 +13,11 @@ class Solution:
                 res.append(lock[0:i] + str((int(lock[i])-1+10)%10) + lock[i+1:])
             return res
         # adding deadends into set as we have to avoid visting them
-        visited = set(deadends)
+        visited = set()
+        deadends = set(deadends)
+        '''
+            Apply dfs over the decision tree and at each level for the target to find the turns in shortest path
+        '''
         queue = deque()
         # add the root lock state and init moves
         queue.append(["0000",0])
@@ -26,7 +30,7 @@ class Solution:
                 return move
             for child_state in get_children(lock_state):
                 # check if already visited or part of deadend
-                if child_state not in visited:
+                if child_state not in visited and child_state not in deadends:
                     visited.add(child_state)
                     queue.append([child_state,move+1])
         return -1
