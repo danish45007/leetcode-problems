45bf9711159d9c22013cc436171c18e48bd7473d
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-07-02 15:02:34 +0530

    Time: 29 ms (94.95%), Space: 13.9 MB (12.71%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,12 +1,16 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        num_of_tree = [1] * (n+1)
-        
+        number_of_trees = [1]*(n+1) #[nodes [0,n]]
+        number_of_trees[0] = 1
+        number_of_trees[1] = 1
+        # starting from node 2 to n+1
         for node in range(2,n+1):
-            total = 0
+            total_trees = 0
+            # for each node iterate over from 1 node+1 where in each itertion act as root
             for root in range(1,node+1):
-                left_node = root - 1
-                right_node = node - root
-                total += num_of_tree[left_node]*num_of_tree[right_node]
-                num_of_tree[node] = total
-        return num_of_tree[n]
+                left_count = root - 1
+                right_count = node - root
+                total_trees += number_of_trees[left_count] * number_of_trees[right_count]
+            
+            number_of_trees[node] = total_trees
+        return number_of_trees[n]
