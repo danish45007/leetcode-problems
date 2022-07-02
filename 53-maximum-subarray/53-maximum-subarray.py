84fa1dcc374353caf1598fafcef6a152cf5b31c5
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-07-02 17:14:59 +0530

    Time: 995 ms (57.59%), Space: 28.5 MB (9.61%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        sub_string_sum = [0]*len(nums)
-        sub_string_sum[0] = nums[0]
+        sub_array_sum = [0]*len(nums)
+        sub_array_sum[0] = nums[0]
         for i in range(1,len(nums)):
-            sub_string_sum[i] = max(sub_string_sum[i-1]+nums[i],nums[i])
-        return max(sub_string_sum)
+            # out of 2 decisions taking the max of two
+            # d1 adding the curr number to prev sum
+            # d2 starting from curr number
+            sub_array_sum[i] = max(sub_array_sum[i-1]+nums[i],nums[i])
+        return max(sub_array_sum)
         
