155e3d1b53ccd2e508579285318b8962edb86409
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-07-05 17:54:10 +0530

    Time: 77 ms (36.81%), Space: 14 MB (82.80%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -7,20 +7,13 @@ class Solution:
         end = len(nums) - 1
         while start <= end:
             mid = start + (end-start) // 2
-            if(mid > 0 and mid < (len(nums)-1)):
-                if(nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]):
-                    return mid
-                elif(nums[mid+1] > nums[mid]):
-                    start = mid + 1
-                elif(nums[mid-1] > nums[mid]):
-                    end = mid - 1
-            elif(mid == 0):
-                if(nums[0] > nums[1]):
-                    return 0
-                else:
-                    return 1
-            elif(mid == len(nums) - 1):
-                if(nums[len(nums)-1] > nums[len(nums)-2]):
-                    return len(nums) - 1
-                else:
-                    return len(nums) - 2
+            mid_val,left_of_mid,right_of_mid = nums[mid],nums[mid-1] if mid > 0 else float('-inf'),nums[mid+1] if mid < len(nums)-1 else float('-inf')
+            # found peak
+            if left_of_mid < mid_val > right_of_mid:
+                return mid
+            elif left_of_mid < mid_val < right_of_mid:
+                start = mid + 1
+            else:
+                end = mid - 1
+                
+                
