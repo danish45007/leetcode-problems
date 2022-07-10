c7542e8041c36ebcb1a91dde8c491225954f4c44
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2022-07-10 15:09:43 +0530

    Time: 1088 ms (9.11%), Space: 21.3 MB (7.62%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -2,6 +2,7 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         row,col = len(matrix),len(matrix[0])
         dp = {} #{(r,c) => LIP}
+        directions = [(0,1),(0,-1),(-1,0),(1,0)]
         def dfs(r,c,prev):
             # out of bound or non inc order base case
             if r < 0 or r == row or c < 0 or c == col or prev >= matrix[r][c]:
@@ -14,15 +15,13 @@ class Solution:
             # apply dfs in all 4 direction
             # atleast 1 if no LIP
             res = 1
-            res = max(res,1 + dfs(r+1,c,matrix[r][c]))
-            res = max(res,1 + dfs(r-1,c,matrix[r][c]))            
-            res = max(res,1 + dfs(r,c-1,matrix[r][c]))            
-            res = max(res,1 + dfs(r,c+1,matrix[r][c]))
+            for inc_row,inc_col in directions:
+                res = max(res,1+dfs(r+inc_row,c+inc_col,matrix[r][c]))
             # cache result
             dp[(r,c)] = res
             return res
-        
+        max_lip = float('-inf')
         for r in range(row):
             for c in range(col):
-                dfs(r,c,-1)
-        return max(dp.values())
+                max_lip = max(dfs(r,c,-1),max_lip)
+        return max_lip
